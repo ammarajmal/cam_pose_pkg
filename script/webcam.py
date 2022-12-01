aae3ba1284f26616ec598477cb405dc17a223a31
Ammar Ajmal <ammarajml@gmail.com>
2022-12-02 03:39:01 +0900

    fps correction

--- script/webcam.py
+++ script/webcam.py
@@ -3,19 +3,24 @@
 # *********************
 # webcam video stream
 # *********************
-import cv2
 import time
+import cv2
+
 # 0 - Dell Webcam
 
-cap = cv2.VideoCapture('filename.avi')
+# cap = cv2.VideoCapture('filename.avi')
+cap = cv2.VideoCapture(0)
 pTime = 0
 while (cap.isOpened()):
+
     ret, frame = cap.read()
     if ret == True:
         cTime = time.time()
         fps = int(1/(cTime - pTime))
         cv2.putText(frame, f'FPS: {int(fps)}', (420, 40), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 1), 3)
         cv2.imshow('Frame', frame)
+        pTime = cTime
+        print(fps)
         if cv2.waitKey(25) & 0xFF == ord('q'):  break
     else:   break
 cap.release()
